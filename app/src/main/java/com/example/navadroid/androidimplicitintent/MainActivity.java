40a92f21b46549611bb4720be420d3a54a563e40
MSI <pairsir@outlook.com>
2017-11-23 21:23:49 +0700

    for homework

--- app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
+++ app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
@@ -6,6 +6,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 // Ideas from https://www.formget.com/android-intent/
 // and https://developer.android.com/guide/components/intents-common.html
@@ -38,16 +39,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.btn_intent_2:
-                // TODO: ???
+                 //ver1 call
+                // intent.setAction(Intent.ACTION_DIAL);
+                // intent.setData(Uri.parse("tel:0993959365"));
+                // ver2 call
+                intent.setAction(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:0993959365"));
+
+                startActivity(intent);
                 break;
             case R.id.btn_intent_3:
-                // TODO: ???
+                //TODO: Open URL in brower
+              //  Uri webpage = Uri.parse("https:www.google.co.th");
+              //  Intent intent1 = new Intent(Intent.ACTION_VIEW, webpage);
+
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https:www.google.co.th"));
+
+                startActivity(intent);
+
                 break;
             case R.id.btn_intent_4:
-                // TODO: ???
+                // TODO: CAMERA
+                intent.setAction(MediaStore.INTENT_ACTION_VIDEO_CAMERA);
+                startActivity(intent);
                 break;
             case R.id.btn_intent_5:
-                // TODO: ???
+                intent.setType("text/plain");
+                intent.setData(Uri.parse("mailto:someone_who_waiting_for_love@example.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Why you still miss him?");
+                intent.putExtra(Intent.EXTRA_TEXT, "Just open your eyes, better guy might be around you");
+                startActivity(Intent.createChooser(intent, "Send email via"));
                 break;
 
         }
